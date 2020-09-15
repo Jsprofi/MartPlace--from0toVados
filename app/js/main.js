b0010fbc183f4b1dce0be1147bf4b97d4932b3e8
Jsprofi <Jsprofi@yandex.ru>
2020-09-15 12:38:20 +0300

    temp

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,23 @@ $(function() {
 
 
 
-  $(".rate-star").rateYo({
-    rating: 3.6,
-    starWidth: "12px",
-    readOnly: true
-  });
+    $(".rate-star").rateYo({
+        rating: 3.6,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $(".weekly__title-sliderbtn").slick({
+        arrows: false,
+        dots: true
+    });
+
+    $(".weekly__products-slider-inner").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+    });
 
 
 
