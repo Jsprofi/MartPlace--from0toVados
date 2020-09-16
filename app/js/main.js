d13d4da9b7dd9d0a337d93eecd220fe1e36547f6
JsProfi <jsprofi@yandex.ru>
2020-09-16 08:07:08 +0300

    16.09.2020 init

--- app/js/main.js
+++ app/js/main.js
@@ -2,23 +2,26 @@ $(function() {
 
 
 
-    $(".rate-star").rateYo({
-        rating: 3.6,
-        starWidth: "12px",
-        readOnly: true
-    });
+  $(".rate-star").rateYo({
+    rating: 3.6,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-    $(".weekly__title-sliderbtn").slick({
-        arrows: false,
-        dots: true
-    });
+  // $(".weekly__title-sliderbtn").slick({
+  //     arrows: false,
+  //     dots: true
+  // });
 
-    $(".weekly__products-slider-inner").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+  $(".weekly__products-slider-inner").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class = "slick-arrow slick-prev"> <span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class = "slick-arrow slick-next"> <span class="lnr lnr-chevron-right"></span></button>'
 
-    });
+  });
 
 
 
